ba6eb26c96f9f11f23524c0810f2b6b07d3bbdec
coco <colin.regalmezin@gmail.com>
2026-03-21 12:23:55 +0100

    Start to add animation/decoration + remove anki key/username

--- configuration.nix
+++ configuration.nix
@@ -99,7 +99,7 @@
   programs.hyprland = {
     enable = true;
     withUWSM = true; # recommended for most users
-    xwayland.enable = true; # Xwayland can be disabled.
+    # xwayland.enable = true; # Xwayland can be disabled.
   };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
--- users/coco/home.nix
+++ users/coco/home.nix
@@ -33,7 +33,6 @@
   programs.chromium.enable = true;
   programs.btop.enable = true;
   programs.hyprlock.enable = true;
-  programs.vscode.enable = true;
 
   programs.anki = {
     enable = true;
@@ -42,8 +41,8 @@
       sync = {
         autoSync = true;
         syncMedia = true;
-        usernameFile = "/home/coco/dotfiles/anki/username";
-        keyFile = "/home/coco/dotfiles/anki/key";
+        usernameFile = "/home/coco/anki/username";
+        keyFile = "/home/coco/anki/key";
       };
     };
     addons = [
@@ -84,10 +83,51 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
+	# xwayland.enable = true;
     systemd.enable = false;
 
     extraConfig = ''
       monitor = DP-3, 3440x1440@164.90Hz,0x0,1	
+
+decoration {
+	rounding = 10
+
+}
+
+animations {
+	enabled = yes
+
+    #        NAME,           X0,   Y0,   X1,   Y1
+    bezier = easeOutQuint,   0.23, 1,    0.32, 1
+    bezier = easeInOutCubic, 0.65, 0.05, 0.36, 1
+    bezier = linear,         0,    0,    1,    1
+    bezier = almostLinear,   0.5,  0.5,  0.75, 1
+    bezier = quick,          0.15, 0,    0.1,  1
+
+    #           NAME,          ONOFF, SPEED, CURVE,        [STYLE]
+    animation = global,        1,     10,    default
+    animation = border,        1,     5.39,  easeOutQuint
+    animation = windows,       1,     4.79,  easeOutQuint
+    animation = windowsIn,     1,     4.1,   easeOutQuint, popin 87%
+    animation = windowsOut,    1,     1.49,  linear,       popin 87%
+    animation = fadeIn,        1,     1.73,  almostLinear
+    animation = fadeOut,       1,     1.46,  almostLinear
+    animation = fade,          1,     3.03,  quick
+    animation = layers,        1,     3.81,  easeOutQuint
+    animation = layersIn,      1,     4,     easeOutQuint, fade
+    animation = layersOut,     1,     1.5,   linear,       fade
+    animation = fadeLayersIn,  1,     1.79,  almostLinear
+    animation = fadeLayersOut, 1,     1.39,  almostLinear
+    animation = workspaces,    1,     1.94,  almostLinear, fade
+    animation = workspacesIn,  1,     1.21,  almostLinear, fade
+    animation = workspacesOut, 1,     1.94,  almostLinear, fade
+    animation = zoomFactor,    1,     7,     quick
+
+}
+
+misc {
+	disable_hyprland_logo = true
+}
 
       input {
         kb_layout = fr
