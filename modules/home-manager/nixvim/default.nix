8b8fe5727a2a7f8940a4540f072d78265cc2c693
coco <colin.regalmezin@gmail.com>
2026-03-27 19:56:13 +0100

    Create basic nixvim conf, need to add more plugins later

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -1,19 +1,40 @@
 { inputs, pkgs, ... }:
 {
-  imports = [ inputs.nixvim.homeModules.nixvim ];
+  imports = [
+    inputs.nixvim.homeModules.nixvim
+  ];
 
   programs.nixvim = {
+    imports = [
+      ./snacks.nix
+    ];
     enable = true;
+    defaultEditor = true;
     colorschemes.catppuccin.enable = true;
     opts = {
       # Use system clipboard
       clipboard = "unnamedplus";
 
       # Tabuluation settings
-      tabstop = 4;
-      softtabstop = 4;
-      shiftwidth = 4;
+      tabstop = 2;
+      softtabstop = 2;
+      shiftwidth = 2;
       expandtab = true;
+      autoindent = true;
+      smartindent = true;
+
+      number = true;
+      wrap = true;
+
+    };
+    plugins = {
+      # UI plugins
+      noice.enable = true;
+      lualine.enable = true;
+      web-devicons.enable = true;
+      bufferline.enable = true;
+
+      cmp.enable = true;
     };
   };
 }
---
+++ modules/home-manager/nixvim/snacks.nix
@@ -0,0 +1,12 @@
+{
+  plugins.snacks = {
+    enable = true;
+    settings = {
+      animate.enabled = true;
+      notifier.enabled = true;
+      explorer.enabled = true;
+      indent.enabled = true;
+      terminal.enabled = true;
+    };
+  };
+}
